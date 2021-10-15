78d599938eaac7e7524cf8269d362d704de8c0aa
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2021-10-15 23:46:07 +0300

    added style

--- scripts/timer.js
+++ scripts/timer.js
@@ -24,28 +24,28 @@ class CountdownTimer {
     const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
     const secs = Math.floor((time % (1000 * 60)) / 1000);
 
-    refs.days.textContent = days;
+    refs.days.textContent = String(days).padStart(2, "0");
     if (days === 1) {
       refs.daysLabel.textContent = "Day";
     } else if (refs.daysLabel.textContent === "Day") {
       refs.daysLabel.textContent = "Days";
     }
 
-    refs.hours.textContent = hours;
+    refs.hours.textContent = String(hours).padStart(2, "0");
     if (hours === 1) {
       refs.hoursLabel.textContent = "Hour";
     } else if (refs.hoursLabel.textContent === "Hour") {
       refs.hoursLabel.textContent = "Hours";
     }
 
-    refs.mins.textContent = mins;
+    refs.mins.textContent = String(mins).padStart(2, "0");
     if (mins === 1) {
       refs.minsLabel.textContent = "Minute";
     } else if (refs.minsLabel.textContent === "Minute") {
       refs.minsLabel.textContent = "Minutes";
     }
 
-    refs.secs.textContent = secs;
+    refs.secs.textContent = String(secs).padStart(2, "0");
     if (secs === 1) {
       refs.secsLabel.textContent = "Second";
     } else if (refs.secsLabel.textContent === "Second") {
